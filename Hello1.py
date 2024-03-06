f7b580e303c9f98265219cd89183c14c977acad0
Shubham Yadav <yadavshubham1052003@gmail.com>
2024-03-07 00:21:32 +0530

    'first commit'

--- Hello1.py
+++ Hello1.py
@@ -1,2 +1,26 @@
-print("Hello Shubham Yadav")
+#print("Hello Shubham Yadav")
 
+#print("Shubham yadav")
+#print(10+20)
+
+#print(10)
+# variables in Python
+
+#age=21
+#old=False
+#a=None
+#name='shubham'
+#max=100.23
+
+#print(type(age))
+#print(type(old))
+#print(type(a))
+#print(type(name))
+#print(type(max))
+
+# multi line comment 
+'''a=10
+b=20
+print(a+b)'''
+
+print("Shubham")
