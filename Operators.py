dfe30d0559344057404190478eb32c0c39f04621
Shubham Yadav <yadavshubham1052003@gmail.com>
2024-03-08 00:22:43 +0530

    first commit

---
+++ Operators.py
@@ -0,0 +1,43 @@
+#Arithmetic 
+
+'''a=4
+b=3
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
+
+print(a**b)'''
+
+# relational operator 
+
+'''a=50
+b=29
+print(a==b)
+print(a>b)
+print(a<b)
+print(a!=b)
+print(a>=b)'''
+
+#assignment operator
+
+'''a=20
+a+=10
+a-=5
+a*=2
+a/=2
+print(a)'''
+
+#logical operator
+a=40
+b=10
+print(not (a>b))
+print(not True)
+
+val1=True
+val2=False
+print("and Operator :", val1 and val2)
+
+print("or Operator :", val1 or val2)
---
+++ TypeConversion.py
@@ -0,0 +1,24 @@
+'''a=2
+b=4.25
+sum=a+b
+
+# autotamtically convert : 
+# jab ek data type dusre se bada ho to python
+# automatically convert kar deta hai small ko big me
+
+print(sum)
+
+# type casting jab hum chhote ko bade me daalna chah rahe ho tab
+
+a=int("2")
+b=4.9
+print(type(a))
+print(a+b) '''
+
+# input : in Python
+
+name=input("What's Your Name: ")
+age= input("enter age: ")
+cpi= input("enter cpi:")
+
+print(name,age,cpi)
