696f9d16d1ad6d14052e7f9f9e401a725f72170a
Shubham Yadav <yadavshubham1052003@gmail.com>
2024-03-13 23:28:15 +0530

    first commit

---
+++ Loop.py
@@ -0,0 +1,28 @@
+# loops are used to repeat instructions
+
+# while loops 
+
+# syntax : while condition :
+# some work
+
+'''count=1
+
+while(count<=5):
+    print("Hello")
+    count=count+1'''
+
+# print numbers from 1 to 5
+
+'''i=1
+
+while(i<=5):
+    print(i)
+    i+=1'''
+
+# reverse print 
+i=5
+
+while(i>=1):
+    print(i)
+    i-=1
+    
---
+++ PracticeWhile.py
@@ -0,0 +1,57 @@
+# print number from 1 to 100
+
+'''i=1
+while i<=100:
+
+    print(i)
+    i+=1'''
+
+#2: print number from 100 to 1'''
+'''i=100
+
+while i>=1:
+    print(i)
+    i-=1'''
+
+
+#3: print multiplication table of number n
+
+#n=input("Enter the number")
+'''
+here we have to focus when i am taking input it takes as string
+ so before performing any operation convert it into required data type 
+
+n=int(input("Enter the number))
+i=1
+
+while i<=10:
+
+    print(n*i)
+    i+=1'''
+
+#4. print the elements of the following list using loop
+'''list=[1,4,9,16,2,3,4,5,7]
+i=0
+
+while i<len(list):
+    print(list[i])
+    i+=1'''
+
+#5. Search for a number x in this tuple using loop:
+
+x=5
+
+tuple=(1,2,3,4,5,6,7)
+
+i=0
+
+while i<len(tuple):
+    if (tuple[i]==x):
+        print("Present",i)
+        
+    i+=1
+
+    
+
+
+
