bf46be983aaac07e30e13904080a841ab6d055da
Shubham Yadav <yadavshubham1052003@gmail.com>
2024-04-07 19:25:29 +0530

    first commit

--- Reduce.py
+++ Reduce.py
@@ -1,5 +1,44 @@
-from functools import reduce
+'''from functools import reduce
 a=[10,20,30,40,50,100]
 
 result= reduce(lambda n,m:n+m,a)
-print(result)
+print(result)'''
+
+# list and it's method
+
+'''l1= [1,2,3,4]
+x=l1.pop(2)
+print(x)
+
+del l1[0]
+print(l1)
+
+l1.remove(2)
+print(l1)
+
+l1.insert(2,5)
+print(l1)
+l1.reverse()
+print(l1)
+
+l1.sort()
+print(l1)
+
+l1.sort(reverse=True)
+print(l1)
+print(l1)
+z=l1.index(4)
+print(z)
+l2=[10,11,14]
+l2[0]=50
+l1.extend(l2)
+print(l1)'''
+
+
+def student(firstname, lastname):
+    print(firstname, lastname)
+ 
+ 
+# Keyword arguments
+student(firstname='Geeks', lastname='Practice')
+student(lastname='Practice', firstname='Geeks')
--- tuples.py
+++ tuples.py
@@ -1,8 +1,50 @@
-tuple=(2,3,4,5,6)
+tuple=(1,2,3)
 print(tuple)
-print(tuple[0])
-# tuple[0]=1 : not allowed in tuple
+print(type(tuple))
 
-print(tuple.index(2))
+t2=(10,)
+print(t2)
+print(type(t2))
 
-print(tuple.count(5))
+t3=(1,2,5,7)
+print(t3[0])
+print(t3[1])
+print(t3[2])
+print(t3[3])
+# out of range : print(t3[4])
+
+# method-2 accessing tuple element
+
+i=0
+while i < len(t3):
+      print(t3[i])
+      i+=1
+# method-3
+
+for a in t3 :
+      print(a)      
+
+# built in methods in tuple
+print(len(t3))
+# min and max
+
+print(min(t3))
+
+print(max(t3))
+
+# sum return sum of all value of tuple
+print(sum(t3))
+
+#sorted
+print(sorted(t3))
+print(sorted(t3, reverse=True))
+
+print(t3*3)
+print(t2+t3)
+
+print(t3.index(5))
+print(t3.count(5))
+
+t5=(1,2,3,4,5,6,7,8)
+print(t5[2:6:1])
+print(t5[::-1])
