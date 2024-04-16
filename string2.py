69f6f0238d09b823ff26f1fae6b18a2dc6539bab
Shubham Yadav <yadavshubham1052003@gmail.com>
2024-04-16 10:37:04 +0530

    first commit

---
+++ string2.py
@@ -0,0 +1,18 @@
+s1="my name is shubham"
+print(type(s1))
+print(len(s1))
+print(s1.index('shu'))
+print(s1.count('h'))
+print(s1.startswith('my'))
+print(s1.upper())
+print(s1.lower())
+print(s1.replace('m','M'))
+print(s1[0:len(s1)])
+# slicing 
+# -1 last index 
+
+print(s1[1:5])
+print(s1[0:5:1])
+print(s1[:5:-1])
+s2='123'
+print(s2.isdigit())
