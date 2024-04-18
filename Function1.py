ba3e6661b97edf22533606f44c373962bbf232d8
Shubham Yadav <yadavshubham1052003@gmail.com>
2024-04-18 20:31:41 +0530

    first commit

---
+++ Function1.py
@@ -0,0 +1,34 @@
+'''def add():
+
+    # take nothing return nothing 
+    a= int(input("Enter Number :"))
+    b= int(input("Enter Second Nymber :"))
+    c=a+b
+    print("Sum is",c)
+add()    '''
+
+# take something return nothing 
+'''def add(a,b):
+
+    c=a+b
+
+    print("Sum is :",c)
+add(10,20)  '''
+
+'''def add():
+
+    a= int (input("Enter first number :"))
+    b= int (input("Enter Second Number "))
+
+    c= a+b
+    return c
+sum= add()
+print("Sum is", sum)'''
+
+def add(a,b):
+
+    return a+b
+add(10,20)
+
+
+
